20324c525d33965c4009d1e90496e6d792a8e5fc
TranDanh1122 <trandanh14042000@gmail.com>
2024-12-20 08:53:37 +0700

    add reset form funtion

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ let timer
 let validateRule = {
     first_name: value => value,
     last_name: value => value,
-    // Validate email format: must contain '@' and '.' with no spaces
     email: value => value && (/^[^@\s]+@[^@\s]+\.[^@\s]+$/).test(value),
-    content: value => value 
+    content: value => value
 }
 let toggleError = (el) => document.querySelector(`[name="${el}"]`).closest(`fieldset`).toggleAttribute('error', true);
+let resetForm = () => {
+    form.reset();
+    document.querySelectorAll('fieldset[error]').forEach(el => el.toggleAttribute('error', false));
+}
 let handleSubmit = (e) => {
     e.preventDefault();
     let data = Object.fromEntries(new FormData(form));
@@ -26,9 +29,11 @@ let handleSubmit = (e) => {
             state = false;
         }
     });
-
     card.toggleAttribute('success', state);
-    clearTimeout(timer)
-    timer = setTimeout(() => card.toggleAttribute('success', false), 5000)
+    if (state) {
+        resetForm()
+        clearTimeout(timer)
+        timer = setTimeout(() => card.toggleAttribute('success', false), 5000)
+    }
 }
 form.addEventListener('submit', handleSubmit);
